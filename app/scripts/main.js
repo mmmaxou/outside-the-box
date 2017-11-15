8b47ebc26cf6d33ee4ecab642d6bf5761473d4f8
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2017-11-15 15:57:27 +0100

    adding points and text throwing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var WORDS = []
+
 $(document).ready(function () {
 
   var granimInstance = new Granim({
@@ -34,11 +36,13 @@ $(document).ready(function () {
           gameLogic.start()
         })
     })
+    .click()
 
   $('#game-text-input').on('keyup', function (e) {
     gameInput.onInput(e)
     return false
   })
+
 })
 
 function GameLogic() {
@@ -50,8 +54,7 @@ function GameLogic() {
   self.start = function () {
     self.running = true
 
-    // fetch 2 words to always have something to type
-    self.fetchWord()
+    // fetch 500 words to always have something to type
     self.fetchWord()
   }
 
@@ -60,18 +63,21 @@ function GameLogic() {
   self.fetchWord = function () {
     const minLen = 3
     const maxLen = 8
+    const limit = 500
 
     $.ajax({
-        url: `http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=${minLen}&maxLength=${maxLen}&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
+        url: `http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=${minLen}&maxLength=${maxLen}&limit=${limit}&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
       })
       .done(function (data) {
         // Satanize the word fetched to escape special chars
-        var word = data.word
-        word = word.replace(/\W/g, '')
-        gameInput.addWord(word)
+        data.forEach(function (obj) {
+          var word = obj.word
+          word = word.replace(/\W/g, '')
+          WORDS.push(word)
+        })
+
+        gameInput.addWord()
       })
-
-
   }
 
   return self
@@ -96,20 +102,18 @@ function GameInput() {
   }
 
   // Add a new word
-  self.addWord = function (word) {
-    if (word && typeof word == "string") {
-      self.wordsToAvoid.push(new Word(word, self.uniqId++))
-    } else {
-      console.error("Wrong input: ", word)
-    }
+  self.addWord = function () {
+    var word = getRandomArrayElement(WORDS)
+    self.wordsToAvoid.push(new Word(word, self.uniqId++))
+    animateBar(self.wordsToAvoid[0].avgTime)
   }
 
   // Delete the first word on self.wordsToAvoid
   self.deleteFirstWord = function () {
 
     self.computeScore()
-
-    // Delete
+    self.addWord()
+      // Delete
     self.wordsToAvoid.shift()
 
   }
@@ -120,17 +124,21 @@ function GameInput() {
     let w = self.wordsToAvoid[0]
     let timeTaken = w.endTimecode - w.startTimecode
     timeTaken = Math.round(timeTaken)
-    let seconds = (timeTaken / 1000).toFixed(2)
+    let seconds = Number((timeTaken / 1000).toFixed(2))
 
     // Compute score
     let len = w.text.length
+    let delta = seconds - w.avgTime
 
-    // average char per seconds
-    const avgTypingSpeed = 200 / 60
 
-    /// TODO
-
-    let score = timeTaken / l
+    // HARDCODED multiplier
+    const MULTIPLIER = 10
+    let score = 1
+    if (delta < 0) {
+      score += (-delta) * MULTIPLIER
+      score = Math.round(score)
+      throwText("+" + score)
+    }
     score = Math.round(score)
     self.score += score
 
@@ -140,12 +148,33 @@ function GameInput() {
     $('#score').text("Score: " + self.score)
   }
 
+  // Animate Progress bar
+  self.$bar = $('#bar')
+
+
+  function animateBar(time) {
+    clearInterval(self.id)
+    time = time * 1000 // Time in milliseconds
+    const TIME_INTERVAL = 100
+    const NB_INTERVAL = time / TIME_INTERVAL
+    let width = 100
+    let decrementValue = width / NB_INTERVAL
+    self.id = setInterval(function frame() {
+      if (width <= 0) {
+        clearInterval(self.id)
+      } else {
+        width -= decrementValue
+        self.$bar.css('width', width + '%')
+      }
+    }, TIME_INTERVAL);
+  }
 
   gameLogic.input = self
   return self
 }
 
 function Word(text, id) {
+
   var self = {
     text: text || "unknown",
     DOM_selector: '.word-list',
@@ -153,10 +182,16 @@ function Word(text, id) {
     nextCharId: 0,
     startTimecode: performance.now(),
     endTimecode: undefined,
+    avgTime: undefined,
     /// Hardcoded parent ///
     parent: gameInput,
     logic: gameLogic,
   }
+
+  /// Compute it's avgTime
+  const avgTypingSpeed = 1 / (200 / 60)
+  const EASIER = 1.5
+  self.avgTime = (self.text.length * avgTypingSpeed) + EASIER
 
   /// Graphical interface
   $(self.DOM_selector).append(`<li id=${id} >${text}</li>`)
@@ -185,7 +220,6 @@ function Word(text, id) {
       if (self.nextCharId == self.text.length) {
         // Delete the word
         self.endTimecode = performance.now()
-        self.logic.fetchWord()
         self.parent.deleteFirstWord()
         self.delete()
       } else {
@@ -200,3 +234,66 @@ function Word(text, id) {
 
 var gameLogic = new GameLogic()
 var gameInput = new GameInput()
+
+
+
+/* HELPERS */
+function getRandomArrayElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+var Text = {
+  queue: [],
+  throwText: function (text = "0x") {
+    this.queue.push(text)
+    this.dequeue()
+  },
+  run: function (text = "0x") {
+    var id = new Date()
+    id = id.getTime()
+    $('body')
+      .append("<span class='throwText' id='" + id + "'>" + text + "</div>")
+    $('#' + id)
+      .css('color', getRandomColor())
+      .css('font-size', 80 + randInt(-30, 30))
+    setTimeout(function () {
+      $('#' + id)
+        .remove()
+    }, 700)
+  },
+  dequeue: function () {
+    if (this.queueing) {
+      return
+    }
+    this.queueing = true
+    var self = this
+    this.id = setInterval(function () {
+      var t = self.queue.shift()
+      if (!t) {
+        self.queueing = false
+        clearInterval(self.id)
+      } else {
+        self.run(t)
+      }
+    }, 300)
+  },
+  stop: function () {
+    this.queue = []
+  }
+}
+
+function throwText(text = "0x") {
+  Text.throwText(text)
+}
+
+function randInt(min = 0, max = 1) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
