0deade839a42b5b91ba4df31c47d01b9593eaa33
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2017-11-24 12:49:29 +0100

    adding timer, fix bug and adding difficukty

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,258 +2,318 @@ var WORDS = []
 
 $(document).ready(function () {
 
-  var granimInstance = new Granim({
-    element: '#canvas-1',
-    name: 'basic-gradient',
-    direction: 'left-right',
-    opacity: [1, 1],
-    isPausedWhenNotInView: true,
-    states: {
-      'default-state': {
-        gradients: [
+	var granimInstance = new Granim({
+		element: '#canvas-1',
+		name: 'basic-gradient',
+		direction: 'left-right',
+		opacity: [1, 1],
+		isPausedWhenNotInView: true,
+		states: {
+			'default-state': {
+				gradients: [
                 ['#AA076B', '#61045F'],
                 ['#02AAB0', '#00CDAC'],
                 ['#DA22FF', '#9733EE']
             ]
-      }
-    }
-  });
+			}
+		}
+	});
 
-  var triggerTimer = 0
-    // var triggerTimer = 2000
-    /// Trigger the introduction animation on the first Click
-  $('.canvas-interactive-wrapper button')
-    .click(function () {
-      $(this).hide()
-        .prev()
-        .text('Go !')
-        .animate({
-          opacity: 0,
-          'margin-top': '-=1000'
-        }, triggerTimer, function () {
-          /// Animation is complete
-          $('.game').slideDown()
-          gameLogic.start()
-        })
-    })
-    .click()
+	var triggerTimer = 0
+		// var triggerTimer = 2000
+		/// Trigger the introduction animation on the first Click
+	$('.canvas-interactive-wrapper button')
+		.click(function () {
+			$(this).hide()
+				.prev()
+				.text('Go !')
+				.animate({
+					opacity: 0,
+					'margin-top': '-=1000'
+				}, triggerTimer, function () {
+					/// Animation is complete
+					$('.game').slideDown()
+					gameLogic.start()
+				})
+		})
+		//.click()
 
-  $('#game-text-input').on('keyup', function (e) {
-    gameInput.onInput(e)
-    return false
-  })
+	$('#game-text-input').on('keydown', function (e) {
+		gameInput.onInput(e)
+		e.preventDefault()
+		return 0
+	})
+
+	// Link input changes to the difficulty tab
+	$('#difficulty input').on('input', function (e) {
+		let diff = $(this).val()
+		$('span#diff').text("Difficulty: " + diff)
+		gameInput.setDifficulty(diff)
+	})
+
 
 })
 
 function GameLogic() {
-  var self = {}
+	var self = {}
 
-  self.running = false
-  self.input = null
-    // Start the game
-  self.start = function () {
-    self.running = true
+	self.running = false
+	self.input = null
+		// Start the game
+	self.start = function () {
+		self.running = true
 
-    // fetch 500 words to always have something to type
-    self.fetchWord()
-  }
+		// fetch 500 words to always have something to type
+		self.fetchWord()
 
-  // Use Wordnik API to fetch words
-  // http://www.wordnik.com/
-  self.fetchWord = function () {
-    const minLen = 3
-    const maxLen = 8
-    const limit = 500
+		// Start the timer
+		Timer.start()
 
-    $.ajax({
-        url: `http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=${minLen}&maxLength=${maxLen}&limit=${limit}&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
-      })
-      .done(function (data) {
-        // Satanize the word fetched to escape special chars
-        data.forEach(function (obj) {
-          var word = obj.word
-          word = word.replace(/\W/g, '')
-          WORDS.push(word)
-        })
+		// Hide Diff
+		$('#difficulty').slideUp()
+	}
 
-        gameInput.addWord()
-      })
-  }
+	// Use Wordnik API to fetch words
+	// http://www.wordnik.com/
+	self.fetchWord = function () {
+		const minLen = 3
+		const maxLen = 8
+		const limit = 500
 
-  return self
+		$.ajax({
+				url: `http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=${minLen}&maxLength=${maxLen}&limit=${limit}&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
+			})
+			.done(function (data) {
+				// Satanize the word fetched to escape special chars
+				data.forEach(function (obj) {
+					var word = obj.word
+					word = word.replace(/\W/g, '')
+					WORDS.push(word)
+				})
+
+				gameInput.addWord()
+			})
+	}
+
+	return self
 }
 
 function GameInput() {
-  var self = {
-    selector: '#game-text-input',
-    wordsToAvoid: [],
-    timeTaken: [],
-    uniqId: 0,
-    score: 0,
-  }
+	var self = {
+		selector: '#game-text-input',
+		wordsToAvoid: [],
+		timeTaken: [],
+		uniqId: 0,
+		score: 0,
+		difficulty: 4,
+	}
 
 
-  // Input handler called every time a key is pressed
-  self.onInput = function (e) {
-    let value = String.fromCharCode(e.which)
+	// Input handler called every time a key is pressed
+	self.onInput = function (e) {
+		let value = String.fromCharCode(e.which)
 
-    // Try to attack the first word stored using the letter pressed
-    self.wordsToAvoid[0].attack(value)
-  }
+		// Try to attack the first word stored using the letter pressed
+		self.wordsToAvoid[0].attack(value)
+	}
 
-  // Add a new word
-  self.addWord = function () {
-    var word = getRandomArrayElement(WORDS)
-    self.wordsToAvoid.push(new Word(word, self.uniqId++))
-    animateBar(self.wordsToAvoid[0].avgTime)
-  }
+	// Add a new word
+	self.addWord = function () {
+		var word = getRandomArrayElement(WORDS)
+		self.wordsToAvoid.push(new Word(word, self.uniqId++))
+		animateBar(self.wordsToAvoid[0].avgTime)
+	}
 
-  // Delete the first word on self.wordsToAvoid
-  self.deleteFirstWord = function () {
+	// Delete the first word on self.wordsToAvoid
+	self.deleteFirstWord = function () {
 
-    self.computeScore()
-    self.addWord()
-      // Delete
-    self.wordsToAvoid.shift()
+		self.computeScore()
+		self.addWord()
+			// Delete
+		self.wordsToAvoid.shift()
 
-  }
+	}
 
-  self.computeScore = function () {
+	self.computeScore = function () {
 
-    // Compute time taken
-    let w = self.wordsToAvoid[0]
-    let timeTaken = w.endTimecode - w.startTimecode
-    timeTaken = Math.round(timeTaken)
-    let seconds = Number((timeTaken / 1000).toFixed(2))
+		// Compute time taken
+		let w = self.wordsToAvoid[0]
+		let timeTaken = w.endTimecode - w.startTimecode
+		timeTaken = Math.round(timeTaken)
+		let seconds = Number((timeTaken / 1000).toFixed(2))
 
-    // Compute score
-    let len = w.text.length
-    let delta = seconds - w.avgTime
+		// Compute score
+		let len = w.text.length
+		let delta = seconds - w.avgTime
 
-    // HARDCODED multiplier
-    const MULTIPLIER = 10
-    let score = 1
-    if (delta < 0) {
-      score += (-delta) * MULTIPLIER
-      score = Math.round(score)
-      throwText('+' + score)
-    }
+		// HARDCODED multiplier
+		// Depends on the difficulty
+		const MULTIPLIER = map(self.difficulty, 1, 10, 2, 20)
+		let score = 1
+		if (delta < 0) {
+			score += (-delta) * MULTIPLIER
+			score = Math.round(score)
+			throwText('+' + score)
+		}
 
-    // Compute errors
-    const ERR_MULTIPLIER = 5
-    let errors = w.errors
-    score -= errors * ERR_MULTIPLIER
+		// Compute errors
+		const ERR_MULTIPLIER = 5
+		let errors = w.errors
+		score -= errors * ERR_MULTIPLIER
 
-    score = Math.round(score)
-    self.score += score
-
-
-    // Display the time
-    $('#time-taken').text(seconds + ' secs')
-    $('#score').text('Score: ' + self.score)
-  }
-
-  self.animateError = function () {
-    if (self.isAnimating)
-      return
-
-    self.isAnimating = true
-    $(self.selector).addClass('error-animation')
-    setTimeout(function () {
-      $(self.selector).removeClass('error-animation')
-      self.isAnimating = false
-    }, 200)
-  }
-
-  // Animate Progress bar
-  self.$bar = $('#bar')
+		score = Math.round(score)
+		self.score += score
 
 
-  function animateBar(time) {
-    clearInterval(self.id)
-    time = time * 1000 // Time in milliseconds
-    const TIME_INTERVAL = 100
-    const NB_INTERVAL = time / TIME_INTERVAL
-    let width = 100
-    let decrementValue = width / NB_INTERVAL
-    self.id = setInterval(function frame() {
-      if (width <= 0) {
-        clearInterval(self.id)
-      } else {
-        width -= decrementValue
-        self.$bar.css('width', width + '%')
-      }
-    }, TIME_INTERVAL);
-  }
+		// Display the time
+		$('#time-taken').text(seconds + ' secs')
+		$('#score').text('Score: ' + self.score)
+	}
 
-  gameLogic.input = self
-  return self
+	self.animateError = function () {
+		if (self.isAnimating)
+			return
+
+		self.isAnimating = true
+		$(self.selector).addClass('error-animation')
+		setTimeout(function () {
+			$(self.selector).removeClass('error-animation')
+			self.isAnimating = false
+		}, 200)
+	}
+
+	self.setDifficulty = function (diff) {
+		self.difficulty = diff
+	}
+
+	// Animate Progress bar
+	self.$bar = $('#bar')
+
+
+	function animateBar(time) {
+		clearInterval(self.id)
+		time = time * 1000 // Time in milliseconds
+		const TIME_INTERVAL = 100
+		const NB_INTERVAL = time / TIME_INTERVAL
+		let width = 100
+		let decrementValue = width / NB_INTERVAL
+		self.id = setInterval(function frame() {
+			if (width <= 0) {
+				clearInterval(self.id)
+			} else {
+				width -= decrementValue
+				self.$bar.css('width', width + '%')
+			}
+		}, TIME_INTERVAL);
+	}
+
+	gameLogic.input = self
+	return self
 }
 
 function Word(text, id) {
 
-  var self = {
-    text: text || 'unknown',
-    DOM_selector: '.word-list',
-    id: id,
-    nextCharId: 0,
-    startTimecode: performance.now(),
-    endTimecode: undefined,
-    avgTime: undefined,
-    errors: 0,
-    /// Hardcoded parent ///
-    parent: gameInput,
-    logic: gameLogic,
-  }
+	var self = {
+		text: text || 'unknown',
+		DOM_selector: '.word-list',
+		id: id,
+		nextCharId: 0,
+		startTimecode: performance.now(),
+		endTimecode: undefined,
+		avgTime: undefined,
+		errors: 0,
+		/// Hardcoded parent ///
+		parent: gameInput,
+		logic: gameLogic,
+	}
 
-  /// Compute it's avgTime
-  const avgTypingSpeed = 1 / (200 / 60)
-  const EASIER = 1.5
-  self.avgTime = (self.text.length * avgTypingSpeed) + EASIER
+	/// Compute it's avgTime
+	const avgTypingSpeed = 1 / (200 / 60)
+	const EASIER = map(self.parent.difficulty, 1, 10, 2, -0.5)
+	self.avgTime = (self.text.length * avgTypingSpeed) + EASIER
 
-  /// Graphical interface
-  $(self.DOM_selector).append(`<li id=${id} >${text}</li>`)
-  self.update = function () {
+	/// Graphical interface
+	$(self.DOM_selector).append(`<li id=${id} >${text}</li>`)
+	self.update = function () {
 
-    // Change the word using the substring function
-    $(`${self.DOM_selector} #${self.id}`).text(self.text.substr(self.nextCharId))
-  }
-  self.delete = function () {
-    $(`${self.DOM_selector} #${self.id}`).remove()
-  }
+		// Change the word using the substring function
+		$(`${self.DOM_selector} #${self.id}`).text(self.text.substr(self.nextCharId))
+	}
+	self.delete = function () {
+		$(`${self.DOM_selector} #${self.id}`).remove()
+	}
 
-  /// Logic
+	/// Logic
 
-  // Remove the first letter if it is the right one
-  self.attack = function (letter) {
+	// Remove the first letter if it is the right one
+	self.attack = function (letter) {
 
-    /// Check if the input fit
-    if (!letter || typeof letter != 'string') {
-      return
-    }
+		/// Check if the input fit
+		if (!letter || typeof letter != 'string') {
+			return
+		}
 
-    /// Check if the given letter is the right one
-    if (letter.toLowerCase() === self.text.charAt(self.nextCharId).toLowerCase()) {
-      self.nextCharId++;
-      if (self.nextCharId == self.text.length) {
-        // Delete the word
-        self.endTimecode = performance.now()
-        self.parent.deleteFirstWord()
-        self.delete()
-      } else {
-        self.update()
-      }
-    } else {
-      /// It is not the right letter, hence it is an error
-      self.errors++;
-      // Animate the error
-      self.parent.animateError()
+		/// Check if the given letter is the right one
+		if (letter.toLowerCase() === self.text.charAt(self.nextCharId).toLowerCase()) {
+			self.nextCharId++;
+			if (self.nextCharId == self.text.length) {
+				// Delete the word
+				self.endTimecode = performance.now()
+				self.parent.deleteFirstWord()
+				self.delete()
+			} else {
+				self.update()
+			}
+		} else {
+			/// It is not the right letter, hence it is an error
+			self.errors++;
+			// Animate the error
+			self.parent.animateError()
 
-    }
-  }
+		}
+	}
 
 
-  return self
+	return self
+}
+
+var Timer = {
+	minutes: 0,
+	seconds: 0,
+	milliseconds: 0,
+	selector: "#timer",
+	interval: undefined,
+	start() {
+		var n = 7
+		this.interval = setInterval(() => {
+			this.milliseconds += n
+			this.format()
+			this.display()
+		}, n)
+	},
+	display() {
+		let m = this.toFixed(2, this.minutes)
+		let s = this.toFixed(2, this.seconds)
+		let mm = this.toFixed(3, this.milliseconds)
+		$(this.selector).text(`${m}:${s}:${mm}`)
+	},
+	format() {
+		if (this.milliseconds > 1000) {
+			this.milliseconds -= 1000
+			this.seconds++
+		}
+		if (this.seconds > 60) {
+			this.seconds -= 60
+			this.minutes++
+		}
+	},
+	toFixed(n, val) {
+		if (val < Math.pow(10, n - 1)) {
+			return "0" + String(val)
+		} else {
+			return val
+		}
+	}
 }
 
 var gameLogic = new GameLogic()
